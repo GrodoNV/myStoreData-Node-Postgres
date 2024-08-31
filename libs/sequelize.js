e93855427520a5aee9ed70eef211728e34638bb8
GrodoNV <grodrigonvargas@gmail.com>
2024-08-31 08:54:35 -0500

    creando modelo del usuario para el ORM

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,6 +1,7 @@
 const {Sequelize} = require('sequelize');
 
 const { config } = require('./../config/config');
+const setUpModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
@@ -10,5 +11,9 @@ const sequelize = new Sequelize(URI,{
   dialect:'postgres',
   logging: true,
 })
+
+setUpModels(sequelize);
+
+sequelize.sync();
 
 module.exports =sequelize;
