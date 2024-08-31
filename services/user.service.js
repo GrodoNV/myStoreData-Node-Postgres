ba1090dfcad391820d02e2f86539c06ac14bb9b6
GrodoNV <grodrigonvargas@gmail.com>
2024-08-31 11:38:26 -0500

    agregando crud

--- services/user.service.js
+++ services/user.service.js
@@ -6,7 +6,8 @@ class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
@@ -15,17 +16,23 @@ class UserService {
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if(!user){
+      throw boom.notFound('user not found');
+    }
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const rta = user.update(changes);
+
+    return rta;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
     return { id };
   }
 }
